e4060efd4d0d826a28b71f2fbec9f65ac0586f62
bennage <christopher@bennage.com>
2011-08-12 17:06:49 -0700

    began playing with everyauth

--- app.js
+++ app.js
@@ -4,11 +4,97 @@
  */
 
 var express = require('express');
+var everyauth = require('everyauth');
 var PostProvider = require('./PostProvider').PostProvider;
 
 var posts = new PostProvider('localhost', 27017);
 
 var app = module.exports = express.createServer();
+
+// everyauth stuff
+var fb = {
+  appId : '12373185169',
+  appSecret: 'b990335e26ff08a408bb7ebff709f584'
+};
+
+everyauth.debug = true;
+
+everyauth.everymodule.findUserById( function(id,callback){
+  // debugger;
+  callback(null, usersById[id]);
+});  
+
+everyauth.facebook
+  .appId(fb.appId)
+  .appSecret(fb.appSecret)
+  .findOrCreateUser( function(session, accessToken, accessTokenExtra, fbUserMetadata){
+      // debugger;
+      return {};
+  })
+  .redirectPath('/');
+
+everyauth
+  .password
+    .loginWith('email')
+    .getLoginPath('/login')
+    .postLoginPath('/login')
+    .loginView('login.jade')
+//    .loginLocals({
+//      title: 'Login'
+//    })
+//    .loginLocals(function (req, res) {
+//      return {
+//        title: 'Login'
+//      }
+//    })
+    .loginLocals( function (req, res, done) {
+      setTimeout( function () {
+        done(null, {
+          title: 'Async login'
+        });
+      }, 200);
+    })
+    .authenticate( function (login, password) {
+      var errors = [];
+      if (!login) errors.push('Missing login');
+      if (!password) errors.push('Missing password');
+      if (errors.length) return errors;
+      var user = usersByLogin[login];
+      if (!user) return ['Login failed'];
+      if (user.password !== password) return ['Login failed'];
+      return user;
+    })
+
+    .getRegisterPath('/register')
+    .postRegisterPath('/register')
+    .registerView('register.jade')
+//    .registerLocals({
+//      title: 'Register'
+//    })
+//    .registerLocals(function (req, res) {
+//      return {
+//        title: 'Sync Register'
+//      }
+//    })
+    .registerLocals( function (req, res, done) {
+      setTimeout( function () {
+        done(null, {
+          title: 'Async Register'
+        });
+      }, 200);
+    })
+    .validateRegistration( function (newUserAttrs, errors) {
+      var login = newUserAttrs.login;
+      if (usersByLogin[login]) errors.push('Login already taken');
+      return errors;
+    })
+    .registerUser( function (newUserAttrs) {
+      var login = newUserAttrs[this.loginKey()];
+      return usersByLogin[login] = addUser(newUserAttrs);
+    })
+
+    .loginSuccessRedirect('/')
+    .registerSuccessRedirect('/');
 
 // Configuration
 
@@ -18,6 +104,7 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(everyauth.middleware());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -43,8 +130,6 @@ var types = {
   say: function() {},
   think: function() {},
   move: function() {},
-  roll: roll,
-  d: roll
 };
 
 function processPost(data) {
@@ -88,44 +173,7 @@ app.get('/stream', function(req, res){
   posts.findAll( function(error,docs){ res.json(docs); });
 });
 
-function roll(dice)
-{
-    dice = dice.replace(/- */,'+ -');
-    dice = dice.replace(/D/,'d');
-
-    var re = / *\+ */;
-    var items = dice.split(re);
-    var res = [];
-    var type = [];
-
-    items.forEach(function(item) {
-        var match = item.match(/^[ \t]*(-)?(\d+)?(?:(d)(\d+))?[ \t]*$/);
-        if (match) {
-            var sign = match[1]?-1:1;
-            var num = parseInt(match[2] || "1", 10);
-            var max = parseInt(match[4] || "0", 10);
-            if (match[3]) {
-                for ( j=1; j<=num; j++) {
-                    res[res.length] = sign * Math.ceil(max*Math.random());
-                    type[type.length] = max;
-                }
-            }
-            else {
-                res[res.length] = sign * num;
-                type[type.length] = 0;
-            }
-        } 
-        else {
-          return null;
-        }
-    });
-
-    if (res.length === 0) {
-      return null;
-    }
-    return res + ' ' + type;
-}
-
+// everyauth.helpExpress(app);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
