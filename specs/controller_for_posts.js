0a5f790d57d8154bb40199ff6b3dcfbd0fa9b630
bennage <christopher@bennage.com>
2011-08-20 22:30:46 -0700

    relocated a test

---
+++ specs/controller_for_posts.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var controller = require('../controllers/post');
+
+module.exports = {
+    'the index returns a set of posts': function(){
+		//todo: how can I mock out mongoose? or Post?   
+		assert.true(true);     
+    },
+}
