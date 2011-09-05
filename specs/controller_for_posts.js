2c19a925ebd2afc566f38ce9143fe4d293910e9b
bennage <christopher@bennage.com>
2011-09-05 00:03:18 -0700

    trying to learn how to write unit tests

--- specs/controller_for_posts.js
+++ specs/controller_for_posts.js
@@ -1,17 +1,63 @@
+var horaa = require('horaa');
 var assert = require('assert');
-var mongoose = require('mongoose');
-var schema = require('../models/schema');
+
+// var helper = horaa('../helper');
+// helper.hijacking('restrict',function(){
+// 	console.log('restrict');
+// });
+
+//do the hijacking
+var mongoose = horaa('mongoose');
+mongoose.hijack('model', function(type) {
+    switch(type) {
+	case 'User':
+	break;
+	case 'Post': 
+	return {
+		count: function(conditions, callback){
+			callback(null,100);
+		},
+		find: function() {
+			}
+		};
+		}
+    }
+    console.log(type);
+});
+
 var controller = require('../controllers/post');
+
 
 module.exports = {
 	
 	teardown: function(done){
-       mongoose.disconnect();
+       // mongoose.disconnect();
        done();
     },
 
-    'the index returns a set of posts': function(){
-		//todo: how can I mock out mongoose? or Post?   
-		assert.ok(true);     
+    'the index returns an empty page when no campaign is in the context': function(){
+		controller.index[1](
+			{ context: {}},
+			{ json: function(data){
+				assert.equal(0,data.count);
+				assert.equal(0,data.page);
+				assert.equal(0,data.results.length);
+			}});
+    },
+
+    'the index returns ...': function(){
+    	var campaignId = 999;
+
+
+
+		controller.index[1](
+			{ context: {
+				campaign: { id: campaignId }
+			}},
+			{ json: function(data){
+				assert.equal(0,data.count);
+				assert.equal(0,data.page);
+				assert.equal(0,data.results.length);
+			}});
     },
 }
