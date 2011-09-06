44d32a43273ce18ff58076ceb1bceb10d427419c
bennage <christopher@bennage.com>
2011-09-05 22:01:22 -0700

    made a basic mock for a Mongoose model, not sure how I feel about it yet

--- specs/controller_for_posts.js
+++ specs/controller_for_posts.js
@@ -1,63 +1,77 @@
 var horaa = require('horaa');
 var assert = require('assert');
 
-// var helper = horaa('../helper');
-// helper.hijacking('restrict',function(){
-// 	console.log('restrict');
-// });
+function mockQuery() { 
+}
+
+mockQuery.prototype.find = function(criteria) {
+	this.criteria = criteria;
+	return this;
+};
+
+mockQuery.prototype.skip = function(count) {
+	this.skip = count;
+	return this;
+};
+
+mockQuery.prototype.limit = function(count) {
+	this.limit = count;
+	return this;
+};
+
+mockQuery.prototype.desc = function(property) {
+	this.desc = property;
+	return this;
+};
+
+mockQuery.prototype.count = function(criteria, callback) {
+	var howMany = this.howMany || 0; 
+	callback(null, howMany);
+};
+
+mockQuery.prototype.exec = function(fn) {
+	fn(null,[]);
+};
 
 //do the hijacking
 var mongoose = horaa('mongoose');
 mongoose.hijack('model', function(type) {
-    switch(type) {
-	case 'User':
-	break;
-	case 'Post': 
-	return {
-		count: function(conditions, callback){
-			callback(null,100);
-		},
-		find: function() {
-			}
-		};
-		}
-    }
-    console.log(type);
+	return new mockQuery();
 });
 
 var controller = require('../controllers/post');
-
+var indexAction = controller.index[1];
 
 module.exports = {
 	
-	teardown: function(done){
-       // mongoose.disconnect();
-       done();
-    },
+	'the index returns an empty page when no campaign is in the context': function(){
+		var req = { context: {}};
+		indexAction( req,
+			{ 
+				json: function(data){
+					assert.equal(0,data.count);
+					assert.equal(0,data.page);
+					assert.equal(0,data.results.length);
+				}
+			});
+	},
 
-    'the index returns an empty page when no campaign is in the context': function(){
-		controller.index[1](
-			{ context: {}},
-			{ json: function(data){
-				assert.equal(0,data.count);
-				assert.equal(0,data.page);
-				assert.equal(0,data.results.length);
-			}});
-    },
+	'the index returns ...': function(){
+		var campaignId = 999;
+		var req = {
+				context: {
+					campaign: { id: campaignId }
+				},
+				params: {}
+			};
 
-    'the index returns ...': function(){
-    	var campaignId = 999;
-
-
-
-		controller.index[1](
-			{ context: {
-				campaign: { id: campaignId }
-			}},
-			{ json: function(data){
-				assert.equal(0,data.count);
-				assert.equal(0,data.page);
-				assert.equal(0,data.results.length);
-			}});
-    },
-}
+		indexAction(req,
+			{ 
+				json: function(data){
+					assert.equal(0,data.count,'broken' +  typeof(data.count));
+					assert.equal(1,data.page);
+					assert.equal(0,data.results.length);
+				}
+			});
+	},
+};
